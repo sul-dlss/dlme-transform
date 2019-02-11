50b28a02a8798ed710f50a1ec8ceef02ee0e0206
Justin Littman <justinlittman@stanford.edu>
2019-02-11 07:33:52 -0500

    Adds optional fetching of configs and data from github.
    closes #2

--- invoke.sh
+++ invoke.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 set -e
 
+if [ $USE_GITHUB = "true" ]; then
+  echo "Getting github.com/sul-dlss/dlme"
+  curl -L https://github.com/sul-dlss/dlme/archive/master.zip > master.zip
+  unzip master.zip dlme-master/lib/*
+  mv dlme-master/lib .
+  rm -fr dlme-master
+  rm master.zip
+
+  # Necessary until DLME extracted from rails.
+  sed -i 's/delegate/# delegate/g' lib/traject/dlme_json_resource_writer.rb
+
+
+  echo "Getting github.com/sul-dlss/dlme-metadata"
+  curl -L https://github.com/sul-dlss/dlme-metadata/archive/master.zip > master.zip
+  unzip master.zip
+  mv dlme-metadata-master dlme-metadata
+  rm master.zip
+fi
+
 traject -Ilib -w Traject::JsonWriter $@ | tee output/output.json
