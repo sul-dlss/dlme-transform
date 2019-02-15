0a4dd6539e1b0c0dc46660ef9d3416fcd825e9ed
Justin Coyne <jcoyne@justincoyne.com>
2019-02-15 12:29:42 -0600

    Add class docs to DlmeJsonResourceWriter

--- lib/dlme_json_resource_writer.rb
+++ lib/dlme_json_resource_writer.rb
@@ -5,6 +5,9 @@ require 'adjust_cardinality'
 require 'dlme_json_schema'
 
 # Write the traject output to newline delmitited json
+# This writer also casts fields that should be singular (like `id`) from lists.
+# Finally, it runs the DLME IR schema validator on each output to ensure the config
+# is writing a compliant message.
 class DlmeJsonResourceWriter < Traject::LineWriter
   def serialize(context)
     attributes = context.output_hash.dup
