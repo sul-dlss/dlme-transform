edf6fc8c2143eea00a88942357c9dacf5af85f56
Justin Coyne <jcoyne@justincoyne.com>
2019-02-26 11:11:46 -0600

    Drop the methods that are now in TrajectPlus

--- lib/macros/fgdc.rb
+++ lib/macros/fgdc.rb
@@ -3,14 +3,12 @@
 module Macros
   # Macros for extracting FGDC values from Nokogiri documents
   module FGDC
-    NS = { fgdc: 'http://www.fgdc.gov/metadata/fgdc-std-001-1998.dtd' }.freeze
-
     def normalize_type
       extract_fgdc('/*/idinfo/citation/citeinfo/geoform', translation_map: 'types')
     end
 
     def harvard_uuid(record)
-      urls = record.xpath('/*/idinfo/citation/citeinfo/onlink', NS).map(&:text)
+      urls = record.xpath('/*/idinfo/citation/citeinfo/onlink', TrajectPlus::Macros::FGCD::NS).map(&:text)
       urls.first.split('CollName=')[1]
     end
 
@@ -40,11 +38,6 @@ module Macros
     def select_identifier(record, context)
       uuid = extract_uuid(record, context)
       uuid.presence || default_identifier(context)
-    end
-
-    # @param xpath [String] the xpath query expression
-    def extract_fgdc(xpath, options = {})
-      extract_xml(xpath, NS, options)
     end
   end
 end
