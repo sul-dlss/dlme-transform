7a8cc11e4c0dfcb561bf6e437f083f89f469f0da
Justin Coyne <jcoyne@justincoyne.com>
2019-02-27 09:14:25 -0600

    Fix MetThumbnailFetcher

--- lib/dlme_utils.rb
+++ lib/dlme_utils.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 require 'active_support/benchmarkable'
+require 'faraday'
 
 module DLME
   # Misc. utilities for working with DLME data
   module Utils
     extend ActiveSupport::Benchmarkable
 
-    def self.caching_faraday_client
+    def self.client
       Faraday.new do |builder|
-        builder.use :http_cache, store: Rails.cache
+        # builder.use :http_cache, store: Rails.cache
 
         builder.adapter Faraday.default_adapter
       end
@@ -17,9 +18,12 @@ module DLME
 
     def self.fetch_json(uri)
       resp = benchmark("DLME::Utils.fetch_json(#{uri})", level: :debug) do
-        caching_faraday_client.get uri
+        client.get uri
       end
-      ::JSON.parse(resp.body) if resp.success?
+      resp_content_type = resp.headers['content-type']
+      raise "Unexpected response type '#{resp_content_type}' for #{uri}" unless resp_content_type == 'application/json'
+
+      ::JSON.parse(resp.body)
     end
 
     def self.logger
