96997e6ae1afa0cac7c01aa0c0e6d06a37f965f6
Jacob Hill <jacobhill.mail@gmail.com>
2019-07-30 10:52:13 -0400

    update comments

--- lib/macros/michigan.rb
+++ lib/macros/michigan.rb
@@ -3,13 +3,12 @@
 module Macros
   # Macros for extracting Stanford Specific MODS values from Nokogiri documents
   module Michigan
-    # Removes all but the first value from accumulator, if more values were present.
+    # Removes all but the last value from accumulator, if more values were present.
     #
     # @example
-    #     to_field "main_author", extract_marc("100"), first_only
+    #     to_field "main_author", extract_marc("100"), last_only
     def last_only
       lambda do |_rec, acc|
-        # kind of esoteric, but slice used this way does mutating first, yep
         acc.slice!(0, acc.length - 1)
       end
     end
