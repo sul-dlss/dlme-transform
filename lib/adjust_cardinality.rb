ca0443f451e0b4dc6fa8496f3ca254c7d8f50857
aaron-collier <aaron.collier@stanford.edu>
2019-10-01 09:07:39 -0700

    Fix rubocop

--- lib/adjust_cardinality.rb
+++ lib/adjust_cardinality.rb
@@ -21,7 +21,8 @@ class AdjustCardinality
   end
 
   def flatten_top_level(attributes)
-    flatten = %w[id agg_data_provider agg_data_provider_country agg_provider agg_provider_country agg_is_shown_at agg_is_shown_by agg_preview]
+    flatten = %w[id agg_data_provider agg_data_provider_country agg_provider agg_provider_country
+                 agg_is_shown_at agg_is_shown_by agg_preview]
     attributes.except(*flatten).tap do |output|
       flatten.each do |field|
         next unless attributes.key?(field)
