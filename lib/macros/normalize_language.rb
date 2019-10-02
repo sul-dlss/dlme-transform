6b947d01d783007a48c67d0b0938a14c5800ab9e
Jacob Hill <jacobhill.mail@gmail.com>
2019-10-02 11:54:56 -0700

    rebase and remove cruft in normalize language macro

--- lib/macros/normalize_language.rb
+++ lib/macros/normalize_language.rb
@@ -3,38 +3,19 @@
 module Macros
   # Macros for normalizing incoming metadata
   module NormalizeLanguage
-
-
     # Maps extracted language values to a series of tranlation maps
     # @example
     #   normalize_language => lambda { ... }
     # @return [Proc] a proc that traject can call for each record
     def normalize_language
-      # map = Traject::TranslationMap.new('not_found', 'languages')
-      lambda do |record, accumulator|
-        accumulator.map!{|str| str.downcase}
-        translation_map = ['not_found', 'languages'].map do |spec|
+      lambda do |_record, accumulator|
+        accumulator.map!(&:downcase)
+        translation_map = %w[not_found languages marc_languages
+                             turkish-languages iso_639-1 iso_639-2
+                             iso_639-3 auc-languages-errors].map do |spec|
           Traject::TranslationMap.new(spec)
         end.reduce(:merge)
         translation_map.translate_array!(accumulator)
-          # s = Traject::TranslationMap.new('marc_languages')[str] #.translate_array!(accumulator)
-          # v = Traject::TranslationMap.new('not_found')[s]
- #.translate_array!(accumulator)
-        # }
-      #   accumulator.map! do |val|
-      #     v = val.downcase
-        # Traject::TranslationMap.new('marc_languages').translate_array!(accumulator)
-        # Traject::TranslationMap.new('not_found').translate_array!(accumulator)
-      #   #     lang
-      #     end
-      #   # accumulator << TranslationMap.new('not_found', 'languages')
-      # #     q = map[v]
-      #   end
-        # accumulator.map!{|str| str.downcase}
-        # # accumulator.map! do |str|
-        # #   str.downcase
-        # translation_map('not_found', 'languages')
-        # # end
       end
     end
   end
