513919e8fb619a891ad67e9dc1a23821745d9b7b
Justin Coyne <jcoyne@justincoyne.com>
2019-10-09 11:43:05 -0400

    Refactor NormalizeLanguage for readability

--- lib/macros/normalize_language.rb
+++ lib/macros/normalize_language.rb
@@ -3,6 +3,15 @@
 module Macros
   # Macros for normalizing incoming metadata
   module NormalizeLanguage
+    TRANSFORMS = %w[not_found
+                    languages
+                    marc_languages
+                    turkish-languages
+                    iso_639-1
+                    iso_639-2
+                    iso_639-3
+                    auc-languages-errors].freeze
+
     # Maps extracted language values to a series of tranlation maps
     # @example
     #   normalize_language => lambda { ... }
@@ -10,12 +19,10 @@ module Macros
     def normalize_language
       lambda do |_record, accumulator|
         accumulator.map!(&:downcase)
-        translation_map = %w[not_found languages marc_languages
-                             turkish-languages iso_639-1 iso_639-2
-                             iso_639-3 auc-languages-errors].map do |spec|
-          Traject::TranslationMap.new(spec)
-        end.reduce(:merge)
-        translation_map.translate_array!(accumulator)
+        TRANSFORMS
+          .map { |spec| Traject::TranslationMap.new(spec) }
+          .reduce(:merge)
+          .translate_array!(accumulator)
       end
     end
   end
