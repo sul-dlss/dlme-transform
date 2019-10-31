c18f6ee44c658e03b461b631ea96d9320ef2c7aa
Naomi Dushay <ndushay@stanford.edu>
2019-10-31 13:55:24 -0700

    use common config for sakip-sabanci colls

--- traject_configs/sakip_sabanci_common_config.rb
+++ traject_configs/sakip_sabanci_common_config.rb
@@ -31,7 +31,6 @@ to_field 'cho_coverage', extract_oai('dc:coverage'), strip
 to_field 'cho_creator', extract_oai('dc:creator'),
          strip, split('.')
 to_field 'cho_date', extract_oai('dc:date[1]'), strip
-# to_field 'cho_date_range_norm', extract_oai('dc:date[1]'), strip
 to_field 'cho_description', extract_oai('dc:description'), strip
 to_field 'cho_dc_rights', extract_oai('dc:rights'), strip
 to_field 'cho_edm_type', extract_oai('dc:type'),
