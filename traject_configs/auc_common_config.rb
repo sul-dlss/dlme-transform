c258eaa26c17ecc61d212b7b7c38f5919509472b
Naomi Dushay <ndushay@stanford.edu>
2019-11-05 10:12:09 -0800

    auc_common_config shouldn't have date range fields defined in it

--- traject_configs/auc_common_config.rb
+++ traject_configs/auc_common_config.rb
@@ -33,8 +33,6 @@ to_field 'cho_coverage', extract_oai('dc:coverage'), strip
 to_field 'cho_creator', extract_oai('dc:creator'),
          strip, split('.')
 to_field 'cho_date', extract_oai('dc:date'), strip
-to_field 'cho_date_range_norm', extract_oai('dc:date'), strip, auc_date_range
-to_field 'cho_date_range_hijri', extract_oai('dc:date'), strip, auc_date_range, hijri_range
 to_field 'cho_description', extract_oai('dc:description'), strip
 to_field 'cho_dc_rights', extract_oai('dc:rights'), strip
 to_field 'cho_edm_type', extract_oai('dc:type'),
