268deb69ab085767d502f33d367aca643fcf25bb
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-06 11:42:08 -0800

    Fix BnF validation errors

--- traject_configs/bnf_ifao_config.rb
+++ traject_configs/bnf_ifao_config.rb
@@ -7,3 +7,10 @@ to_field 'cho_creator', extract_srw('dc:creator[3]'), split('.'), first_only, st
 to_field 'cho_edm_type', extract_srw('dc:type'),
          default('notated music'), first_only, strip, transform(&:downcase), translation_map('not_found', 'types', 'french-types')
 to_field 'cho_language', extract_srw('dc:language'), first_only, strip, translation_map('marc_languages')
+
+each_record convert_to_language_hash(
+  'cho_contributor',
+  'cho_creator',
+  'cho_edm_type',
+  'cho_language'
+)
