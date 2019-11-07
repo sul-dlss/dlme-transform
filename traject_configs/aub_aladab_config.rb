780597cdbf05491341730543ea0d2b3ca26b5ddc
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-07 09:54:04 -0800

    Revise AUB mapping, fixes #346

--- traject_configs/aub_aladab_config.rb
+++ traject_configs/aub_aladab_config.rb
@@ -1,3 +1,30 @@
 # frozen_string_literal: true
 
-to_field 'cho_publisher', literal('Dar al-Adab')
+# Cho Required
+to_field 'cho_title', extract_oai('dc:title'), strip, lang('ar-Arab')
+
+# Cho Other
+to_field 'cho_contributor', extract_oai('dc:contributor'),
+         strip, split('.'), lang('ar-Arab')
+to_field 'cho_creator', extract_oai('dc:creator'),
+         strip, split('.'), lang('ar-Arab')
+to_field 'cho_dc_rights', extract_oai('dc:rights'), strip, lang('ar-Arab')
+to_field 'cho_description', extract_oai('dc:description'), strip, lang('ar-Arab')
+to_field 'cho_has_type', literal('Periodical'), lang('en')
+to_field 'cho_has_type', literal('Periodical'), translation_map('norm_has_type_to_ar'), lang('ar-Arab')
+to_field 'cho_is_part_of', extract_oai('dc:source'), strip, lang('ar-Arab')
+to_field 'cho_publisher', literal('Dar al-Adab'), lang('en')
+to_field 'cho_publisher', literal('دار الأدب'), lang('ar-Arab')
+to_field 'cho_subject', extract_oai('dc:subject'), strip, lang('ar-Arab')
+
+each_record convert_to_language_hash(
+  'cho_contributor',
+  'cho_creator',
+  'cho_dc_rights',
+  'cho_description',
+  'cho_has_type',
+  'cho_is_part_of',
+  'cho_publisher',
+  'cho_subject',
+  'cho_title'
+)
