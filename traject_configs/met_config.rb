4d3bd3f2d24ab4bcd29386a1ad207deffa08657e
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-07 17:38:49 -0800

    Revise Met mapping, fixes #350

--- traject_configs/met_config.rb
+++ traject_configs/met_config.rb
@@ -7,8 +7,8 @@ require 'macros/each_record'
 require 'macros/met'
 require 'traject_plus'
 
-extend Macros::DLME
 extend Macros::DateParsing
+extend Macros::DLME
 extend Macros::EachRecord
 extend Macros::Met
 extend TrajectPlus::Macros
@@ -19,28 +19,51 @@ settings do
   provide 'reader_class_name', 'TrajectPlus::JsonReader'
 end
 
-to_field 'agg_provider', provider, lang('en')
-to_field 'agg_provider', provider_ar, lang('ar-Arab')
+# Note: Met JSON uses blanks ("") instead of nulls.
 
-# MET Museum
+# CHO Required
 to_field 'id', extract_json('.objectID'), lambda { |_record, accumulator, context|
   accumulator.map! { |bare_id| identifier_with_prefix(context, bare_id.to_s) }
 }
+to_field 'cho_title', extract_json('.title'), lang('en')
 
-# Note: Met JSON uses blanks ("") instead of nulls.
+# CHO Other
+to_field 'cho_creator', generate_creator, lang('en')
+to_field 'cho_coverage', extract_json('.culture'), transform(&:presence), lang('en')
+to_field 'cho_coverage', extract_json('.dynasty'), transform(&:presence), lang('en')
+to_field 'cho_coverage', extract_json('.excavation'), transform(&:presence), lang('en')
+to_field 'cho_date', generate_object_date, transform(&:presence), lang('en')
+to_field 'cho_date', extract_json('.objectDate'), transform(&:presence), lang('en')
+to_field 'cho_date_range_hijri', met_date_range, hijri_range
+to_field 'cho_date_range_norm', met_date_range
+to_field 'cho_dc_rights', public_domain, lang('en')
+to_field 'cho_dc_rights', extract_json('.rightsAndReproduction'), transform(&:presence), lang('en')
+to_field 'cho_edm_type', literal('Image'), lang('en')
+to_field 'cho_edm_type', literal('Image'), translation_map('norm_types_to_ar'), lang('ar-Arab')
+to_field 'cho_extent', extract_json('.dimensions'), lang('en')
+to_field 'cho_format', extract_json('.objectName'), lang('en')
+to_field 'cho_has_type', literal('Cultural Artifact'), lang('en')
+to_field 'cho_has_type', literal('Cultural Artifact'), translation_map('norm_has_type_to_ar'), lang('ar-Arab')
+to_field 'cho_identifier', extract_json('.accessionNumber')
+to_field 'cho_medium', extract_json('.medium'), lang('en')
+to_field 'cho_provenance', extract_json('.creditLine'), lang('en')
+to_field 'cho_spatial', extract_json('.city'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.country'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.county'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.locale'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.locus'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.region'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.river'), transform(&:presence), lang('en')
+to_field 'cho_spatial', extract_json('.subregion'), transform(&:presence), lang('en')
+to_field 'cho_temporal', extract_json('reign'), transform(&:presence), lang('en')
+to_field 'cho_temporal', extract_json('.period'), transform(&:presence), lang('en')
+to_field 'cho_type', extract_json('.classification'), lang('en')
 
-to_field 'cho_format', extract_json('.objectName')
-to_field 'cho_creator', generate_creator
-to_field 'cho_spatial', extract_json('.city'), transform(&:presence)
-to_field 'cho_type', extract_json('.classification')
-to_field 'cho_spatial', extract_json('.country'), transform(&:presence)
-to_field 'cho_spatial', extract_json('.county'), transform(&:presence)
-to_field 'cho_provenance', extract_json('.creditLine')
-to_field 'cho_coverage', extract_json('.culture'), transform(&:presence)
-to_field 'cho_extent', extract_json('.dimensions')
-to_field 'cho_coverage', extract_json('.dynasty'), transform(&:presence)
-to_field 'cho_coverage', extract_json('.excavation'), transform(&:presence)
-to_field 'cho_dc_rights', public_domain
+# Agg
+to_field 'agg_data_provider', data_provider, lang('en')
+to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
+to_field 'agg_data_provider_country', data_provider_country, lang('en')
+to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(context,
                                   'wr_id' => [extract_json('.objectURL'), transform(&:presence),
@@ -51,31 +74,10 @@ to_field 'agg_preview' do |_record, accumulator, context|
                                   'wr_id' => [extract_json('.primaryImageSmall'), transform(&:presence),
                                               ->(_record, inner_accumulator) { inner_accumulator.compact! }])
 end
-to_field 'cho_spatial', extract_json('.locale'), transform(&:presence)
-to_field 'cho_spatial', extract_json('.locus'), transform(&:presence)
-to_field 'cho_medium', extract_json('.medium')
-to_field 'cho_date', generate_object_date, transform(&:presence)
-to_field 'cho_date', extract_json('.objectDate'), transform(&:presence)
-to_field 'cho_date_range_norm', met_date_range
-to_field 'cho_date_range_hijri', met_date_range, hijri_range
-to_field 'cho_identifier', extract_json('.accessionNumber')
-to_field 'cho_source', extract_json('.accessionNumber')
-to_field 'cho_temporal', extract_json('.period'), transform(&:presence)
-to_field 'cho_spatial', extract_json('.region'), transform(&:presence)
-to_field 'cho_temporal', extract_json('reign'), transform(&:presence)
-to_field 'agg_data_provider', data_provider, lang('en')
-to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
-to_field 'cho_dc_rights', extract_json('.rightsAndReproduction'), transform(&:presence)
-to_field 'cho_spatial', extract_json('.river'), transform(&:presence)
-to_field 'cho_spatial', extract_json('.subregion'), transform(&:presence)
-to_field 'cho_title', extract_json('.title')
-to_field 'cho_edm_type', edm_type
-to_field 'cho_type', literal('Image')
-
+to_field 'agg_provider', provider, lang('en')
+to_field 'agg_provider', provider_ar, lang('ar-Arab')
 to_field 'agg_provider_country', provider_country, lang('en')
 to_field 'agg_provider_country', provider_country_ar, lang('ar-Arab')
-to_field 'agg_data_provider_country', data_provider_country, lang('en')
-to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 
 each_record convert_to_language_hash(
   'agg_data_provider',
