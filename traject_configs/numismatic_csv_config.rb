fd59a02ef7120a5182078f537fc4aa8baede841e
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-08 11:58:18 -0800

    Revise ANS mapping, fixes #347

--- traject_configs/numismatic_csv_config.rb
+++ traject_configs/numismatic_csv_config.rb
@@ -17,42 +17,45 @@ extend TrajectPlus::Macros
 extend TrajectPlus::Macros::Csv
 
 settings do
-  provide 'writer_class_name', 'DlmeJsonResourceWriter'
   provide 'reader_class_name', 'TrajectPlus::CsvReader'
+  provide 'writer_class_name', 'DlmeJsonResourceWriter'
 end
 
 # CHO Required
 to_field 'id', normalize_prefixed_id('RecordId')
-to_field 'cho_title', column('Title')
+to_field 'cho_title', column('Title'), strip, lang('en')
 
 # CHO Other
-to_field 'cho_contributor', column('Authority'), split('|')
-to_field 'cho_coverage', column('Findspot')
-to_field 'cho_creator', column('Mint')
-to_field 'agg_data_provider', data_provider, lang('en')
-to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
-to_field 'cho_date', column('Era')
-to_field 'cho_date', column('Year'), gsub('|', ' - ')
+to_field 'cho_contributor', column('Authority'), split('|'), strip, lang('en')
+to_field 'cho_coverage', column('Findspot'), strip, lang('en')
+to_field 'cho_creator', column('Mint'), strip, lang('en')
+to_field 'cho_date', column('Era'), strip, lang('en')
+to_field 'cho_date', column('Year'), gsub('|', ' - '), strip, lang('en')
 to_field 'cho_date_range_norm', column('Year'), gsub('|', ' - '), parse_range
 to_field 'cho_date_range_hijri', column('Year'), gsub('|', ' - '), parse_range, hijri_range
-to_field 'cho_description', column('Denomination')
-to_field 'cho_description', column('Manufacture')
-to_field 'cho_description', column('Obverse Legend')
-to_field 'cho_description', column('Obverse Type')
-to_field 'cho_description', column('Reverse Legend')
-to_field 'cho_description', column('Reverse Type')
-to_field 'cho_description', column('Weight')
-to_field 'cho_edm_type', literal('Image')
-to_field 'cho_extent', column('Diameter')
-to_field 'cho_format', column('Object Type')
+to_field 'cho_description', column('Denomination'), strip, lang('en')
+to_field 'cho_description', column('Manufacture'), strip, lang('en')
+to_field 'cho_description', column('Obverse Legend'), strip, lang('en')
+to_field 'cho_description', column('Obverse Type'), strip, lang('en')
+to_field 'cho_description', column('Reverse Legend'), strip
+to_field 'cho_description', column('Reverse Type'), strip, lang('en')
+to_field 'cho_description', column('Weight'), strip, lang('en')
+to_field 'cho_edm_type', literal('Image'), lang('en')
+to_field 'cho_edm_type', literal('Image'), translation_map('norm_types_to_ar'), lang('ar-Arab')
+to_field 'cho_extent', column('Diameter'), strip, lang('en')
 to_field 'cho_identifier', column('URI')
 to_field 'cho_identifier', column('RecordId')
-to_field 'cho_medium', column('Material')
-to_field 'cho_source', column('Reference')
-to_field 'cho_spatial', column('Region')
-to_field 'cho_temporal', column('Dynasty')
+to_field 'cho_medium', column('Material'), strip, lang('en')
+to_field 'cho_source', column('Reference'), strip, lang('en')
+to_field 'cho_spatial', column('Region'), strip, lang('en')
+to_field 'cho_temporal', column('Dynasty'), strip, lang('en')
+to_field 'cho_type', column('Object Type'), strip, lang('en')
 
 # Agg
+to_field 'agg_data_provider', data_provider, lang('en')
+to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
+to_field 'agg_data_provider_country', data_provider_country, lang('en')
+to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(context,
                                   'wr_id' => [column('URI')])
@@ -63,11 +66,8 @@ to_field 'agg_preview' do |_record, accumulator, context|
 end
 to_field 'agg_provider', provider, lang('en')
 to_field 'agg_provider', provider_ar, lang('ar-Arab')
-
 to_field 'agg_provider_country', provider_country, lang('en')
 to_field 'agg_provider_country', provider_country_ar, lang('ar-Arab')
-to_field 'agg_data_provider_country', data_provider_country, lang('en')
-to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 
 each_record convert_to_language_hash(
   'agg_data_provider',
