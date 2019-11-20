c37cac65c8e5f7f8fce5ed3ad8e4f87f417832ca
aaron-collier <aaron.collier@stanford.edu>
2019-11-20 10:27:47 -0800

    Add test for timestamp macro

---
+++ spec/lib/traject/macros/timestamp_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'macros/timestamp'
+
+RSpec.describe Macros::Timestamp do
+  let(:klass) do
+    Class.new do
+      include Macros::Timestamp
+    end
+  end
+  let(:instance) { klass.new }
+
+  describe '#timestamp' do
+    let(:time_now) { Time.now }
+    it 'returns a timestamp value' do
+      allow(Time).to receive(:now).and_return(time_now)
+      callable = instance.timestamp
+      expect(callable.call(nil, [])).to eq([time_now])
+    end
+  end
+end
