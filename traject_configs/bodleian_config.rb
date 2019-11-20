346a4b5c8fe4a5bdf04df5be88cdbaa8f64673ec
aaron-collier <aaron.collier@stanford.edu>
2019-11-20 11:12:20 -0800

    Restore bodleian updates

--- traject_configs/bodleian_config.rb
+++ traject_configs/bodleian_config.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+require 'traject_plus'
 require 'dlme_json_resource_writer'
 require 'macros/date_parsing'
 require 'macros/dlme'
 require 'macros/each_record'
+require 'macros/normalize_language'
 require 'macros/timestamp'
 require 'macros/version'
-require 'traject_plus'
 
-extend Macros::DLME
 extend Macros::DateParsing
+extend Macros::DLME
 extend Macros::EachRecord
+extend Macros::NormalizeLanguage
 extend Macros::Timestamp
 extend Macros::Version
 extend TrajectPlus::Macros
@@ -33,22 +35,27 @@ to_field 'id', extract_json('.thumbnail'),
 to_field 'cho_title', extract_json('.title'), strip, default('Untitled Item')
 
 # Cho Other
-to_field 'cho_creator', extract_json('.author'), strip
-to_field 'cho_contributor', extract_json('.printer'), strip, append(' [printer]')
-to_field 'cho_date', extract_json('.date_statement'), strip
+to_field 'cho_creator', extract_json('.author'), strip, lang('en')
+to_field 'cho_contributor', extract_json('.printer'), strip, append(' [printer]'), lang('en')
+to_field 'cho_date', extract_json('.date_statement'), strip, lang('en')
 to_field 'cho_date_range_norm', extract_json('.date_statement'), strip, gsub('/', '-'), parse_range
 to_field 'cho_date_range_hijri', extract_json('.date_statement'), strip, gsub('/', '-'), parse_range, hijri_range
-to_field 'cho_dc_rights', literal('Photo: © Bodleian Libraries, University of Oxford, Terms of use: http://digital.bodleian.ox.ac.uk/terms.html')
-to_field 'cho_description', extract_json('.description'), strip
-to_field 'cho_edm_type', literal('Text')
+to_field 'cho_dc_rights', literal('Photo: © Bodleian Libraries, University of Oxford, Terms of use: http://digital.bodleian.ox.ac.uk/terms.html'), lang('en')
+to_field 'cho_description', extract_json('.description'), strip, lang('en')
+to_field 'cho_edm_type', literal('Text'), lang('en')
+to_field 'cho_edm_type', literal('Text'), translation_map('norm_types_to_ar'), lang('ar-Arab')
+to_field 'cho_has_type', literal('Manuscript'), lang('en')
+to_field 'cho_has_type', literal('Manuscript'), translation_map('norm_types_to_ar'), lang('ar-Arab')
+to_field 'cho_identifier', extract_json('.catalogue_identifier'), strip
+to_field 'cho_language', extract_json('.language'), strip, transform(&:downcase), normalize_language, lang('en')
+to_field 'cho_language', extract_json('.language'), strip, transform(&:downcase), normalize_language, translation_map('norm_languages_to_ar'), lang('ar-Arab')
 to_field 'cho_spatial', extract_json('.place_of_origin'), strip, prepend('Place of Origin: ')
-to_field 'cho_language', extract_json('.language'), strip, transform(&:downcase), translation_map('not_found', 'languages')
 
 # Agg
 to_field 'agg_data_provider', data_provider, lang('en')
 to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
-to_field 'agg_provider', provider, lang('en')
-to_field 'agg_provider', provider_ar, lang('ar-Arab')
+to_field 'agg_data_provider_country', data_provider_country, lang('en')
+to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
@@ -61,11 +68,10 @@ to_field 'agg_preview' do |_record, accumulator, context|
     'wr_id' => [extract_json('.thumbnail'), strip]
   )
 end
-
+to_field 'agg_provider', provider, lang('en')
+to_field 'agg_provider', provider_ar, lang('ar-Arab')
 to_field 'agg_provider_country', provider_country, lang('en')
 to_field 'agg_provider_country', provider_country_ar, lang('ar-Arab')
-to_field 'agg_data_provider_country', data_provider_country, lang('en')
-to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 
 each_record convert_to_language_hash(
   'agg_data_provider',
