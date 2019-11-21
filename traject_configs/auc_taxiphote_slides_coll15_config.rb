7b6fef256a78c7412043de9a7f9099bf8399ba69
Jacob Hill <jacobhill.mail@gmail.com>
2019-11-21 18:56:40 -0500

    Continue AUC mapping

--- traject_configs/auc_taxiphote_slides_coll15_config.rb
+++ traject_configs/auc_taxiphote_slides_coll15_config.rb
@@ -39,16 +39,11 @@ to_field 'id', extract_oai_identifier, strip
 to_field 'cho_title', extract_oai('dc:title[1]'), strip
 
 # Cho Other
-to_field 'cho_contributor', extract_oai('dc:contributor'),
-         strip, split('.')
-to_field 'cho_creator', extract_oai('dc:creator'),
-        strip, split('.')
 to_field 'cho_coverage', extract_oai('dc:coverage'), strip, lang('en')
 to_field 'cho_date', extract_oai('dc:date'), strip, lang('en')
 to_field 'cho_date_range_hijri', extract_oai('dc:date'), strip, auc_date_range, hijri_range
 to_field 'cho_date_range_norm', extract_oai('dc:date'), strip, auc_date_range
 to_field 'cho_dc_rights', extract_oai('dc:rights'), strip, lang('en')
-to_field 'cho_description', extract_oai('dc:description'), strip, lang('en')
 to_field 'cho_edm_type', extract_oai('dc:type'),
          split(';'), strip, transform(&:downcase), normalize_type, lang('en')
 to_field 'cho_edm_type', extract_oai('dc:type'),
@@ -60,7 +55,6 @@ to_field 'cho_language', extract_oai('dc:language'), split(';'),
          split(','), strip, transform(&:downcase), normalize_language, lang('en')
 to_field 'cho_language', extract_oai('dc:language'), split(';'),
          split(','), strip, transform(&:downcase), normalize_language, translation_map('norm_languages_to_ar'), lang('ar-Arab')
-to_field 'cho_publisher', extract_oai('dc:publisher'), strip
 to_field 'cho_relation', extract_oai('dc:relation'), strip, lang('en')
 to_field 'cho_subject', extract_oai('dc:subject'), strip, lang('en')
 to_field 'cho_type', extract_oai('dc:type'), lang('en')
