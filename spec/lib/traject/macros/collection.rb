8f966f261b1e7dbcfae4a0ede84094fbecb524b3
aaron-collier <aaron.collier@stanford.edu>
2019-11-26 14:42:09 -0800

    Add agg_data_provider_collection and corresponding macro

---
+++ spec/lib/traject/macros/collection.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'macros/collection'
+
+RSpec.describe Macros::Collection do
+  let(:klass) do
+    Class.new do
+      include Macros::Collection
+    end
+  end
+  let(:instance) { klass.new }
+
+  describe '#collection' do
+    let(:collection) { 'stanford/maps' }
+    let(:input_name) { 'data/stanford/maps/file_1.xml' }
+    it 'returns a path string that cooresponds to a collection' do
+      context = {}
+      allow(context).to receive(:input_name).and_return(input_name)
+      callable = instance.collection
+      expect(callable.call(nil, [], context)).to eq([collection])
+    end
+  end
+end
