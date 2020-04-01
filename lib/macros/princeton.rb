2d5c2a047c623ae556fac9f46860f47621bb5d8e
Peter Mangiafico <peter@mangiafico.org>
2020-04-01 14:33:42 -0700

    refactor and add macro tests

--- lib/macros/princeton.rb
+++ lib/macros/princeton.rb
@@ -6,26 +6,25 @@ module Macros
     def princeton_title_and_lang
       lambda do |record, accumulator|
         rec_title = record.dig('title') # look for the title
-        if rec_title # we have a title
-          first_rec_title = rec_title.first # just use the first title found
-          if first_rec_title.class == Hash # if we have a value/lang pair in the title field, we can auto set the language
-            title = first_rec_title.dig('@value') # grab the title from @value attribute
-            language = get_title_language(first_rec_title) # get the language from the title
-            script = title.match?(/[aeiou]/) ? 'Latn' : 'Arab'
-            language_and_script = "#{language}-#{script}"
-          else # we do not have a value/lang pair in the title, so have to assume english
-            title = first_rec_title # grab the title
-            language_and_script = 'en'
-          end
-        else # we couldn't find any title at all, use defaults
-          title = 'Untitled'
-          language_and_script = 'none'
-        end
-        accumulator.replace([{ language: language_and_script, values: [title] }])
+        title_and_language = rec_title ? get_title_and_language(rec_title.first) : { language: 'none', values: ['Untitled'] }
+        accumulator.replace([title_and_language])
       end
     end
 
-    def get_title_language(record_title)
+    private
+
+    def get_title_and_language(rec_title)
+      if rec_title.class == Hash # if we have a value/lang pair in the title field, we can auto set the language
+        title = rec_title.dig('@value') # grab the title from @value attribute
+        language = map_language_value(rec_title) # get the language from the title
+        script = title.match?(/[aeiou]/) ? 'Latn' : 'Arab'
+        { language: "#{language}-#{script}", values: [title] }
+      else # we do not have a value/lang pair in the title, so have to assume english
+        { language: 'en', values: [rec_title] }
+      end
+    end
+
+    def map_language_value(record_title)
       lang_map = { ara: 'ar',
                    fas: 'fa',
                    msa: 'ms',
