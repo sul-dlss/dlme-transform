dfff37c7e1c90a03d5a7ea63dc810b56fbb853d5
Jacob Hill <jacobhill.mail@gmail.com>
2020-11-10 15:28:26 -0500

    Add Lausanne mapping

--- lib/macros/date_parsing.rb
+++ lib/macros/date_parsing.rb
@@ -303,6 +303,24 @@ module Macros
       return ParseDate.parse_range(plain_node_value) if plain_node_value
     end
 
+    # Extracts earliest & latest dates from Lausanne record and merges into singe date range value
+    def lausanne_date_range
+      lambda do |record, accumulator, context|
+        first_year = record['from'].to_i if record['from']&.match(/\d+/)
+        last_year = record['to'].to_i if record['to']&.match(/\d+/)
+        accumulator.replace(range_array(context, first_year, last_year))
+      end
+    end
+
+    # Extracts earliest & latest dates from Lausanne record and merges into singe date string value
+    def lausanne_date_string
+      lambda do |record, accumulator|
+        first_year = record['from'] if record['from']&.match(/\d+/)
+        last_year = record['to'] if record['to']&.match(/\d+/)
+        accumulator.replace(["#{first_year} - #{last_year}"]) if record['from'] || record['to']
+      end
+    end
+
     # Extracts earliest & latest dates from Penn museum record and merges into singe date range value
     def penn_museum_date_range
       lambda do |record, accumulator, context|
