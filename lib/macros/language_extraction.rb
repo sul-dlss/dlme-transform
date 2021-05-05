2a63d38c9d4380995818ae84e1cf0d9b208b2b90
Jacob Hill <jacobhill.mail@gmail.com>
2021-05-05 09:57:50 -0400

    Remove cruft

--- lib/macros/language_extraction.rb
+++ lib/macros/language_extraction.rb
@@ -47,26 +47,6 @@ module Macros
       end
     end
 
-    # def arabic_script_lang_or_default(arabic_script_lang, default)
-    #   lambda do |_record, accumulator|
-    #     if accumulator
-    #       ar_values = []
-    #       default_values = []
-    #       accumulator.each do |val|
-    #         lang_code = val.match?(/[ضصثقفغعهخحمنتالبيسشظطذدزرو]/) ? arabic_script_lang : default
-    #         ar_values << val if lang_code == arabic_script_lang
-    #         default_values << val if lang_code == default
-    #       end
-    #       if ar_values.present?
-    #         accumulator.replace([{ language: arabic_script_lang, values: ar_values }])
-    #         accumulator << { language: default, values: default_values } if default_values.present?
-    #       else
-    #         accumulator.replace([{ language: default, values: default_values }])
-    #       end
-    #     end
-    #   end
-    # end
-
     # Returns the value extracted by 'to_field' reformated as a hash with accompanying BCP47 language code.
     # Caution: assumes the language of the metadata field is the same as the main language of the text.
     # Works on cases where the element names are downcased instead of camel cased.
