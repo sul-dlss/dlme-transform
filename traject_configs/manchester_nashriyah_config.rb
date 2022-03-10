0b22ca0dadb6e8687dd7dc66d941615663dc2014
Lauren Sorensen <laurens@nyu.edu>
2022-03-10 11:54:51 -0800

    manchester nashriyah mapping draft

--- traject_configs/manchester_nashriyah_config.rb
+++ traject_configs/manchester_nashriyah_config.rb
@@ -1,107 +1,100 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/string/inflections'
+require 'traject_plus'
 require 'dlme_json_resource_writer'
 require 'dlme_debug_writer'
-require 'macros/collection'
-require 'macros/content_dm'
+require 'macros/csv'
 require 'macros/date_parsing'
 require 'macros/dlme'
 require 'macros/each_record'
-require 'macros/manchester'
+require 'macros/language_extraction'
 require 'macros/normalize_language'
 require 'macros/normalize_type'
 require 'macros/path_to_file'
-require 'macros/oai'
 require 'macros/timestamp'
-require 'macros/title_extraction'
 require 'macros/version'
-require 'traject_plus'
+require 'macros/manchester'
 
-extend Macros::Collection
-extend Macros::ContentDm
+extend Macros::Csv
 extend Macros::DateParsing
 extend Macros::DLME
 extend Macros::EachRecord
-extend Macros::OAI
-extend Macros::Manchester
+extend Macros::LanguageExtraction
 extend Macros::NormalizeLanguage
 extend Macros::NormalizeType
 extend Macros::PathToFile
 extend Macros::Timestamp
-extend Macros::TitleExtraction
 extend Macros::Version
 extend TrajectPlus::Macros
-extend TrajectPlus::Macros::Xml
+extend TrajectPlus::Macros::Csv
+extend Macros::Manchester
 
 settings do
   provide 'writer_class_name', 'DlmeJsonResourceWriter'
-  provide 'reader_class_name', 'TrajectPlus::XmlReader'
+  provide 'reader_class_name', 'TrajectPlus::CsvReader'
 end
 
 # Set Version & Timestamp on each record
 to_field 'transform_version', version
 to_field 'transform_timestamp', timestamp
 
-to_field 'agg_data_provider_collection', literal('manchester-nashriyah'), translation_map('agg_collection_from_provider_id'), lang('en')
-to_field 'agg_data_provider_collection', literal('manchester-nashriyah'), translation_map('agg_collection_from_provider_id'), translation_map('agg_collection_ar_from_en'), lang('ar-Arab')
+# Collection
+to_field 'agg_data_provider_collection', literal('Nashriyah: digital Iranian history (Manchester)'), lang('en')
+to_field 'agg_data_provider_collection', literal('پآرشیو آنلاین نشریات دانشگاه منچستر (منچستر)'), lang('ar-Arab')
 to_field 'agg_data_provider_collection_id', literal('manchester-nashriyah')
 
 # File path
 to_field 'dlme_source_file', path_to_file
 
 # CHO Required
-to_field 'id', extract_oai_identifier, strip
-to_field 'cho_title', xpath_title_or_desc("#{PREFIX}/dc:title[last()]", "#{PREFIX}/dc:description[last()]"), lang('fa-Latn')
-to_field 'cho_title', xpath_title_or_desc("#{PREFIX}/dc:title[1]", "#{PREFIX}/dc:description[1]"), lang('fa-Arab'), default('Untitled', 'بدون عنوان')
+to_field 'id', column('id'), parse_csv, gsub('oai:N/A:Manchester~', '')
+to_field 'cho_title', column('title'), parse_csv, first, lang('fa-Arab')
+to_field 'cho_title', column('title'), parse_csv, odd_only, lang('fa-Latn')
+to_field 'cho_title', column('title'), parse_csv, last, lang('en')
 
 # CHO Other
-to_field 'cho_creator', extract_oai('dc:creator'), strip, lang('fa-Arab')
-to_field 'cho_date', extract_oai('dc:date'), strip, lang('fa-Arab')
-to_field 'cho_date_range_hijri', extract_oai('dc:date'), strip, manchester_solar_hijri_range, hijri_range
-to_field 'cho_date_range_norm', extract_oai('dc:date'), strip, manchester_solar_hijri_range
-to_field 'cho_dc_rights', literal('<a href="https://www.jstor.org/stable/community.28163960?seq=1#metadata_info_tab_contents">"Andishah ha-yi Rastakhiz on JSTOR"</a>'), lang('en')
-to_field 'cho_dc_rights', literal('<a href="https://www.jstor.org/stable/community.28163960?seq=1#metadata_info_tab_contents">"JSTOR اندیشه های رستاخیز در"</a>'), lang('fa-Arab')
-to_field 'cho_description', extract_oai('dc:description'), strip, lang('en')
-to_field 'cho_edm_type', literal('Text'), lang('en')
-to_field 'cho_edm_type', literal('Text'), translation_map('edm_type_ar_from_en'), lang('ar-Arab')
-to_field 'cho_has_type', literal('Serials'), lang('en')
-to_field 'cho_has_type', literal('Serials'), translation_map('has_type_ar_from_en'), lang('ar-Arab')
-to_field 'cho_is_part_of', literal('Nashriyah: digital Iranian history'), lang('en')
-to_field 'cho_is_part_of', literal('آرشیو آنلاین نشریات دانشگاه منچستر'), lang('fa-Arab')
+to_field 'cho_creator', column('creator'), parse_csv, lang('fa-Arab')
+to_field 'cho_date', column('date'), strip, lang('fa-Arab')
+to_field 'cho_date_range_hijri', column('date'), strip, manchester_solar_hijri_range, hijri_range
+to_field 'cho_date_range_norm', column('date'), strip, manchester_solar_hijri_range
+to_field 'cho_dc_rights', column('rights'), parse_csv, lang('en')
+to_field 'cho_description', column('description'), parse_csv, lang('en')
+to_field 'cho_edm_type', column('type'), parse_csv, last, transform(&:downcase), translation_map('has_type_from_contributor'), translation_map('edm_type_from_has_type'), lang('en') # English value
+to_field 'cho_edm_type', column('type'), parse_csv, last, transform(&:downcase), translation_map('has_type_from_contributor'), translation_map('edm_type_from_has_type'), translation_map('edm_type_ar_from_en'), lang('ar-Arab') # Arabic value
+to_field 'cho_format', column('format'), parse_csv, lang('en')
+to_field 'cho_has_type', column('type'), parse_csv, last, transform(&:downcase), translation_map('has_type_from_contributor'), lang('en') # English value
+to_field 'cho_has_type', column('type'), parse_csv, last, transform(&:downcase), translation_map('has_type_from_contributor'), translation_map('has_type_ar_from_en'), lang('ar-Arab') # Arabic value
+
+to_field 'cho_identifier', column('identifier'), parse_csv, last
 to_field 'cho_language', literal('Persian'), lang('en')
 to_field 'cho_language', literal('Persian'), translation_map('lang_ar_from_en'), lang('ar-Arab')
-to_field 'cho_spatial', literal('Iran'), lang('en')
-to_field 'cho_spatial', literal('إيران'), lang('ar-Arab')
-to_field 'cho_type', extract_oai('dc:type'), lang('en')
+to_field 'cho_type', column('type'), parse_csv, last, lang('en')
 
 # Agg
+to_field 'agg_provider', provider, lang('en')
+to_field 'agg_provider', provider_ar, lang('fa-Arab')
+to_field 'agg_provider_country', provider_country, lang('en')
+to_field 'agg_provider_country', provider_country_ar, lang('fa-Arab')
 to_field 'agg_data_provider', data_provider, lang('en')
 to_field 'agg_data_provider', data_provider_ar, lang('ar-Arab')
-
 to_field 'agg_data_provider_country', data_provider_country, lang('en')
-to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
+to_field 'agg_data_provider_country', data_provider_country_ar, lang('fa-Arab')
 to_field 'agg_edm_rights', literal('http://creativecommons.org/licenses/by-nc-sa/4.0')
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
     'wr_edm_rights' => [literal('http://creativecommons.org/licenses/by-nc-sa/4.0')],
-    'wr_id' => [extract_oai_identifier, strip, gsub('oai:N/A:', 'https://luna.manchester.ac.uk/luna/servlet/detail/')],
-    'wr_is_referenced_by' => [extract_oai_identifier, strip, gsub('oai:N/A:', 'https://luna.manchester.ac.uk/luna/servlet/iiif/m/'), append('/manifest')]
+    'wr_id' => [column('identifier'), gsub('oai:N/A:Manchester~', '')]
   )
 end
 to_field 'agg_preview' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
     'wr_edm_rights' => [literal('http://creativecommons.org/licenses/by-nc-sa/4.0')],
-    'wr_id' => [extract_oai('dc:identifier[2]')],
-    'wr_is_referenced_by' => [extract_oai_identifier, strip, gsub('oai:N/A:', 'https://luna.manchester.ac.uk/luna/servlet/iiif/m/'), append('/manifest')]
+    'wr_id' => [column('identifier'), parse_csv, odd_only],
+    'wr_is_referenced_by' => [column('id'), strip, gsub('oai:N/A:', 'https://luna.manchester.ac.uk/luna/servlet/iiif/m/'), append('/manifest')]
   )
 end
-to_field 'agg_provider', provider, lang('en')
-to_field 'agg_provider', provider_ar, lang('ar-Arab')
-to_field 'agg_provider_country', provider_country, lang('en')
-to_field 'agg_provider_country', provider_country_ar, lang('ar-Arab')
 
 each_record convert_to_language_hash(
   'agg_data_provider',
