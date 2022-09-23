b14549a1e314655e982707131b84691780f5fa5f
jacobthill <jacobhill.mail@gmail.com>
2022-09-23 13:41:29 -0400

    move AIMS confic to csv

--- traject_configs/aims_config.rb
+++ traject_configs/aims_config.rb
@@ -4,29 +4,32 @@ require 'dlme_json_resource_writer'
 require 'dlme_debug_writer'
 require 'macros/aims'
 require 'macros/collection'
+require 'macros/csv'
 require 'macros/date_parsing'
 require 'macros/dlme'
 require 'macros/each_record'
 require 'macros/language_extraction'
-require 'macros/normalize_type'
+require 'macros/path_to_file'
 require 'macros/timestamp'
 require 'macros/version'
 require 'traject_plus'
 
-extend Macros::Collection
 extend Macros::AIMS
+extend Macros::Collection
+extend Macros::Csv
 extend Macros::DLME
 extend Macros::DateParsing
 extend Macros::EachRecord
 extend Macros::LanguageExtraction
-extend Macros::NormalizeType
+extend Macros::PathToFile
 extend Macros::Timestamp
 extend Macros::Version
 extend TrajectPlus::Macros
+extend TrajectPlus::Macros::Csv
 
 settings do
   provide 'writer_class_name', 'DlmeJsonResourceWriter'
-  provide 'reader_class_name', 'TrajectPlus::XmlReader'
+  provide 'reader_class_name', 'TrajectPlus::CsvReader'
 end
 
 to_field 'agg_data_provider_collection', literal('aims'), translation_map('agg_collection_from_provider_id'), lang('en')
@@ -38,19 +41,19 @@ to_field 'transform_version', version
 to_field 'transform_timestamp', timestamp
 
 # CHO Required
-to_field 'id', extract_aims('guid'), strip
-to_field 'cho_title', extract_aims('title'), strip, arabic_script_lang_or_default('ar-Arab', 'und-Latn')
+to_field 'id', column('id'), strip
+to_field 'cho_title', column('title'), strip, arabic_script_lang_or_default('ar-Arab', 'und-Latn')
 
 # CHO Other
-to_field 'cho_creator', extract_itunes_aims('author'), strip, lang('en')
-to_field 'cho_date', extract_aims('pubDate'), strip, lang('en')
-to_field 'cho_date_range_norm', extract_aims('pubDate'), strip, parse_range
-to_field 'cho_date_range_hijri', extract_aims('pubDate'), strip, parse_range, hijri_range
+to_field 'cho_creator', column('author'), strip, lang('en')
+to_field 'cho_date', column('pubDate'), strip, lang('en')
+to_field 'cho_date_range_norm', column('pub_date'), strip, parse_range
+to_field 'cho_date_range_hijri', column('pub_date'), strip, parse_range, hijri_range
 to_field 'cho_dc_rights', literal('Use of content for classroom purposes and on other non-profit educational websites is granted (and encouraged) with proper citation.'), lang('en')
-to_field 'cho_description', extract_aims('description'), strip, arabic_script_lang_or_default('ar-Arab', 'und-Latn')
+to_field 'cho_description', column('description'), strip, arabic_script_lang_or_default('ar-Arab', 'und-Latn')
 to_field 'cho_edm_type', literal('Sound'), lang('en')
 to_field 'cho_edm_type', literal('Sound'), translation_map('edm_type_ar_from_en'), lang('ar-Arab')
-to_field 'cho_extent', extract_itunes_aims('duration'), strip, lang('en')
+to_field 'cho_extent', column('extent'), strip, lang('en')
 to_field 'cho_has_type', literal('Interviews'), lang('en')
 to_field 'cho_has_type', literal('Interviews'), translation_map('has_type_ar_from_en'), lang('ar-Arab')
 
@@ -62,13 +65,13 @@ to_field 'agg_data_provider_country', data_provider_country_ar, lang('ar-Arab')
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
-    'wr_id' => [extract_aims('link'), strip]
+    'wr_id' => [column('link'), strip]
   )
 end
 to_field 'agg_preview' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
-    'wr_id' => [extract_thumbnail, transform(&:to_s)]
+    'wr_id' => [column('thumbnail')]
   )
 end
 to_field 'agg_provider', provider, lang('en')
