34258754b76a4422e92ea53789a5fe567f007d2d
Aaron Collier <aaron.collier@stanford.edu>
2023-02-22 16:32:50 -0800

    Fix small bug thatt caused record duplication on subsequent runs in docker

--- invoke.sh
+++ invoke.sh
@@ -5,13 +5,9 @@ DATA_PATH=${DATA_PATH:-$1} # Get the data dir from the environment first if it e
 DATA_DIR=$DATA_PATH
 DATA_DIR=${DATA_DIR//\//-}
 DEBUG_FLAG=$2
-DOWNLOAD_PATH=${DATA_PATH%/*}
-SOURCE_DATA="/opt/airflow/working/$DATA_PATH"
+SOURCE_DATA="/opt/airflow/working"
 METADATA_PATH="/opt/airflow/metadata/$DATA_PATH"
 mkdir -p ${METADATA_PATH}
-
-mkdir -p /opt/traject/data
-cp -R $SOURCE_DATA /opt/traject/data/$DOWNLOAD_PATH
 
 echo "Starting dlme-transform for: ${DATA_PATH}"
 
@@ -19,6 +15,6 @@ OUTPUT_FILENAME="output-$DATA_DIR.ndjson"
 SUMMARY_FILEPATH="output/summary-$DATA_DIR.json"
 
 set +e
-exe/transform --summary-filepath $SUMMARY_FILEPATH --data-dir $DATA_PATH $DEBUG_FLAG | tee "$METADATA_PATH/$OUTPUT_FILENAME"
+exe/transform --summary-filepath $SUMMARY_FILEPATH --base-data-dir $SOURCE_DATA --data-dir $DATA_PATH $DEBUG_FLAG | tee "$METADATA_PATH/$OUTPUT_FILENAME"
 
 echo "Dlme-transform complete for: ${DATA_PATH}"
